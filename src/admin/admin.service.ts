55ad79553815404e20768ead82bc70eaf121a1ea
Prajwal Kakkar <prajwalkakkar>
2022-02-28 13:48:10 +0530

    fix (admin): find one await

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,9 +1,9 @@
 import {
-	HttpException,
-	HttpStatus,
-	Injectable,
-	InternalServerErrorException,
-	NotFoundException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
 } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { InjectRepository } from "@nestjs/typeorm";
@@ -18,74 +18,74 @@ import { ChangePasswordDto } from "./dto/change-password.dto";
 
 @Injectable()
 export class AdminService {
-	constructor(
-		@InjectRepository(Admin) private adminRepository: Repository<Admin>,
-		private jwtService: JwtService
-	) {}
-	async create(createAdminDto: CreateAdminDto) {
-		try {
-			const admin = this.adminRepository.create(createAdminDto);
-			await this.adminRepository.save(admin);
-			return admin;
-		} catch (err) {
-			console.log("Error creating admin", err);
-			throw new InternalServerErrorException();
-		}
-	}
+  constructor(
+    @InjectRepository(Admin) private adminRepository: Repository<Admin>,
+    private jwtService: JwtService
+  ) {}
+  async create(createAdminDto: CreateAdminDto) {
+    try {
+      const admin = this.adminRepository.create(createAdminDto);
+      await this.adminRepository.save(admin);
+      return admin;
+    } catch (err) {
+      console.log("Error creating admin", err);
+      throw new InternalServerErrorException();
+    }
+  }
 
-	async findOne(email: string) {
-		const admin = this.adminRepository.findOne({ where: { email } });
-		if (!admin) throw new NotFoundException();
-		return admin;
-	}
+  async findOne(email: string) {
+    const admin = await this.adminRepository.findOne({ where: { email } });
+    if (!admin) throw new NotFoundException();
+    return admin;
+  }
 
-	remove(id: number) {
-		return `This action removes a #${id} admin`;
-	}
-	async validateUserPassword(email: string, password: string) {
-		const admin = await this.findOne(email);
+  remove(id: number) {
+    return `This action removes a #${id} admin`;
+  }
+  async validateUserPassword(email: string, password: string) {
+    const admin = await this.findOne(email);
 
-		if (!admin)
-			throw new HttpException(
-				"Email or Password is incorrect",
-				HttpStatus.NOT_FOUND
-			);
+    if (!admin)
+      throw new HttpException(
+        "Email or Password is incorrect",
+        HttpStatus.NOT_FOUND
+      );
 
-		const passIsCorrect = await this.checkPassword(password, admin.password);
-		if (admin && passIsCorrect) return admin;
-		else
-			throw new HttpException(
-				"Username or password incorrect",
-				HttpStatus.BAD_REQUEST
-			);
-	}
+    const passIsCorrect = await this.checkPassword(password, admin.password);
+    if (admin && passIsCorrect) return admin;
+    else
+      throw new HttpException(
+        "Username or password incorrect",
+        HttpStatus.BAD_REQUEST
+      );
+  }
 
-	private async checkPassword(
-		pass: string,
-		hashedPass: string
-	): Promise<boolean> {
-		const isPassMatch = await bcrypt.compare(pass, hashedPass);
-		if (!isPassMatch) return false;
-		return true;
-	}
+  private async checkPassword(
+    pass: string,
+    hashedPass: string
+  ): Promise<boolean> {
+    const isPassMatch = await bcrypt.compare(pass, hashedPass);
+    if (!isPassMatch) return false;
+    return true;
+  }
 
-	async login(loginAdminDto: LoginAdminDto) {
-		const { email, password } = loginAdminDto;
-		const result = await this.validateUserPassword(email, password);
-		const payload: JwtPayload = { email: result.email, isAdmin: true };
-		console.log("payload", payload);
-		const accessToken = this.jwtService.sign(payload);
-		console.log("access token", accessToken);
-		return { code: 200, accessToken };
-	}
+  async login(loginAdminDto: LoginAdminDto) {
+    const { email, password } = loginAdminDto;
+    const result = await this.validateUserPassword(email, password);
+    const payload: JwtPayload = { email: result.email, isAdmin: true };
+    console.log("payload", payload);
+    const accessToken = this.jwtService.sign(payload);
+    console.log("access token", accessToken);
+    return { code: 200, accessToken };
+  }
 
-	async changePassword(email: string, changePasswordDto: ChangePasswordDto) {
-		const { newPassword } = changePasswordDto;
-		const admin = await this.findOne(email);
-		if (!admin) throw new HttpException("User not found", HttpStatus.NOT_FOUND);
+  async changePassword(email: string, changePasswordDto: ChangePasswordDto) {
+    const { newPassword } = changePasswordDto;
+    const admin = await this.findOne(email);
+    if (!admin) throw new HttpException("User not found", HttpStatus.NOT_FOUND);
 
-		admin.password = newPassword;
-		await this.adminRepository.save(admin);
-		return { code: 201, message: "Password updated" };
-	}
+    admin.password = newPassword;
+    await this.adminRepository.save(admin);
+    return { code: 201, message: "Password updated" };
+  }
 }
