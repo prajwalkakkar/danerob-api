ea3703f99e52f6dc1349eee919a1179a42d45c15
Prajwal Kakkar <prajwalkakkar>
2022-02-28 14:01:13 +0530

    fix (admin): replace param with query in controller

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Query,
+} from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { AdminService } from "./admin.service";
 import { ChangePasswordDto } from "./dto/change-password.dto";
@@ -8,28 +16,28 @@ import { LoginAdminDto } from "./dto/login-admin.dto";
 @ApiTags("Admin")
 @Controller("admin")
 export class AdminController {
-	constructor(private readonly adminService: AdminService) {}
+  constructor(private readonly adminService: AdminService) {}
 
-	@Post("register")
-	create(@Body() createAdminDto: CreateAdminDto) {
-		return this.adminService.create(createAdminDto);
-	}
+  @Post("register")
+  create(@Body() createAdminDto: CreateAdminDto) {
+    return this.adminService.create(createAdminDto);
+  }
 
-	@Post("login")
-	loginAdmin(@Body() loginAdminDto: LoginAdminDto) {
-		return this.adminService.login(loginAdminDto);
-	}
+  @Post("login")
+  loginAdmin(@Body() loginAdminDto: LoginAdminDto) {
+    return this.adminService.login(loginAdminDto);
+  }
 
-	@Get("get-one")
-	findOne(@Param("email") email: string) {
-		return this.adminService.findOne(email);
-	}
+  @Get("get-one")
+  findOne(@Query("email") email: string) {
+    return this.adminService.findOne(email);
+  }
 
-	@Patch("update-password")
-	update(
-		@Param("email") email: string,
-		@Body() changePasswordDto: ChangePasswordDto
-	) {
-		return this.adminService.changePassword(email, changePasswordDto);
-	}
+  @Patch("update-password")
+  update(
+    @Query("email") email: string,
+    @Body() changePasswordDto: ChangePasswordDto
+  ) {
+    return this.adminService.changePassword(email, changePasswordDto);
+  }
 }
